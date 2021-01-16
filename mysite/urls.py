ff007b9d175fda57bc74a6e3ba2d4671616e6103
daniel <daniel.nemeth83@yahoo.com>
2021-01-16 16:52:28 +0100

    (feat) Course 3 week 3 solution

--- mysite/urls.py
+++ mysite/urls.py
@@ -10,6 +10,8 @@ SITE_ROOT = os.path.join(BASE_DIR, 'site')
 
 urlpatterns = [
     path('', include('home.urls')),
+    path('accounts/', include('django.contrib.auth.urls')),
+    path('autos/', include('autos.urls')),
     path('hello/', include('hello.urls')),
     path('admin/', admin.site.urls),
     path('polls/', include('polls.urls')),
